a9fac154d41d3e04e60db57ecac3d1a45a1df74d
Audrey SOBGOU ZEBAZE <audrey.sobgouzebaze@gmail.com>
2012-07-06 15:57:21 +0200

    Create project

---
+++ DocSetDisplayer/DocSetDisplayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DocSetDisplayer
+//
+//  Created by Audrey SOBGOU ZEBAZE on 06/07/12.
+//  Copyright (c) 2012 TELECOM Saint-Etienne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DocSetDisplayer/DocSetDisplayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DocSetDisplayer
+//
+//  Created by Audrey SOBGOU ZEBAZE on 06/07/12.
+//  Copyright (c) 2012 TELECOM Saint-Etienne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
